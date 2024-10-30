8276301d40c45f1d82403fc0e8df1adc8c3c1730
shikhar <kshikhar94@gmail.com>
2024-10-30 09:45:58 +0530

    image upload api

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import JSONResponse
 import requests
+from PIL import Image
+import io
 
 app = FastAPI()
 
@@ -15,3 +18,32 @@ def ping():
 def get_ip():
     response = requests.get("https://api.ipify.org?format=json")
     return response.json()
+
+@app.post("/upload-image/")
+async def upload_image(file: UploadFile = File(...)):
+    if file.content_type != "image/png":
+        raise HTTPException(status_code=400, detail="only png images are allowed")
+    
+    content = await file.read()
+
+    size = len(content)
+
+    try:
+        image = Image.open(io.BytesIO(content))
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="invalid image file")
+    
+    width, height = image.size
+    format = image.format
+
+    image_info = {
+        "image_name": file.filename,
+        "image_size": size, 
+        "image_format": format,
+        "image_dims": {
+            "width": width,
+            "height": height
+        }
+    }
+
+    return JSONResponse(content=image_info)
